5165b88c99b00569983abe1b941ad21c93b2dd1f
Ninda Khoirunnisa <43130115+nindakhoirunnisa@users.noreply.github.com>
2024-02-13 23:24:00 +0700

    Add files via upload

---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    input: 'C:/Users/acer/OneDrive - The University of Manchester/Coding-dissertation/dissertation/web/static/js/index.js', // Replace with the path to your entry point
+    output: {
+      file: 'C:/Users/acer/OneDrive - The University of Manchester/Coding-dissertation/dissertation/web/static/js/bundle.js', // Replace with the desired output path
+      format: 'iife', // Replace with the desired format (iife, cjs, esm, umd, etc.)
+    },
+  };
